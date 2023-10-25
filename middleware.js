d924658ab603101aea2ca3230d51f508c28f363f
Matheos Profirescu <matheosprofirescu@yahoo.com>
2023-10-25 19:36:16 +0300

    middleware downgrade

--- middleware.js
+++ middleware.js
@@ -3,17 +3,16 @@ import { withAuth } from 'next-auth/middleware'
 import { NextResponse } from 'next/server'
 
 export default withAuth(
+    // `withAuth` augments your `Request` with the user's token.
     function middleware(req) {
-        const { pathname, origin } = req.nextUrl;
-        const { token } = req.nextauth;
+        const { pathname, origin } = req.nextUrl
+        const { token } = req.nextauth
 
-        if (pathname.startsWith('/dashboard') && token?.user?.role !== 'admin') {
-            // Redirecting if the user is not an admin
-            return NextResponse.redirect(origin).setHeader("Access-Control-Allow-Origin", "*");
+        if(
+            pathname.startsWith('/dashboard') && token?.user?.role !== 'admin'
+        ){
+            return NextResponse.redirect(origin)
         }
-
-        // Sending back a normal response with CORS headers
-        return new NextResponse().setHeader("Access-Control-Allow-Origin", "*");
     },
     {
         callbacks: {
